37f539426032ee92219376f83c5fa3baa978dcbb
KaranSingh1301 <70521663+KaranSingh1301@users.noreply.github.com>
2024-07-15 22:55:18 +0530

    lecture 7 todo app 1

---
+++ Lecture 7 Todo App 1/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+//contants
+const app = express();
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+//middlewares
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+//db connection
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("mongodb connected successfully"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  return res.send("Server is up and running...");
+});
+
+//registeration
+app.get("/register", (req, res) => {
+  return res.render("registerPage");
+});
+
+app.post("/register", (req, res) => {
+  console.log(req.body);
+
+  //email and username should be unique
+
+  //userschema
+  //obj
+  //.save
+
+  return res.send("Register success");
+});
+
+//login
+app.get("/login", (req, res) => {
+  return res.render("loginPage");
+});
+
+app.post("/login", (req, res) => {
+  return res.send("login success");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running at: http://localhost:${8000}`);
+});
